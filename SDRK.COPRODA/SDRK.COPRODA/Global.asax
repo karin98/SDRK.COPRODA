4146962fd32b9e0cb56cb7d641603be68d497b13
karin98 <45539307+karin98@users.noreply.github.com>
2018-12-02 12:09:00 -0500

    CREACION PROJECT COPRODA
    APP WEB

---
+++ SDRK.COPRODA/SDRK.COPRODA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SDRK.COPRODA.MvcApplication" Language="C#" %>
---
+++ SDRK.COPRODA/SDRK.COPRODA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SDRK.COPRODA.Startup))]
+namespace SDRK.COPRODA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
